bf52cebf6785405a495657d4bb08f2cd7279927f
Nic Schworer <nic.schworer@gmail.com>
2020-12-06 15:27:43 -0500

    added stuff

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,28 +1,79 @@
-function saveRecord (data) {
-    console.log(data);
+const indexedDB = 
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
     
 
-    let db;
-    const request = indexedDB.open('budget', 1);
+let db;
+const request = indexedDB.open('budget', 1);
 
-    request.onupgradeneeded = ({ target }) => {};
-
-    request.onsuccess = () => {};
-
-
-    // create budget db + bulk (pending) collection into indexedDB
-    // saveRecord -> save to indexedDB
-    // listen to when we are back online and send records
+request.onupgradeneeded = ({ target }) => {
+    db = target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.request)
+};
+
+
+// create budget db + bulk (pending) collection into indexedDB
+// saveRecord -> save to indexedDB
+// listen to when we are back online and send records
+
 
 function saveRecord (record) {
+    const transaction = db.transaction(["pending"], "readwrite");
 
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+
+    // add record to your store with add method.
+    store.add(record);
 };
 
 
-function checkDatabase () {
-
-};
+function checkDatabase() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+  
+          // access your pending object store
+          const store = transaction.objectStore("pending");
+  
+          // clear all items in your store
+          store.clear();
+        });
+      }
+    };
+  }
 
 
 
