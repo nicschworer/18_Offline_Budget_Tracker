72f2c1a63d2c544199971c558bb33f9534db7bc3
Nic Schworer <nic.schworer@gmail.com>
2020-12-11 19:33:53 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
 
 const app = express();
 
@@ -23,6 +22,6 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App running!`);
 });
